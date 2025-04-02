a5c29e805ee4f338e3231a141684ba99fef86336
Vinay Singh <vinaysthakur15@gmail.com>
2025-04-02 13:18:07 +0530

    Daily Coding Challenge DCC 2 April 2025 Maximum Values of an Ordered Triplet 1 Problem

---
+++ DailyCodingChallenge/April25/MaximumValueOfAnOrderedTriplet1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long maximumTripletValue(vector<int>& v) {
+  int n = v.size();
+
+  long long maxA = v[0];
+  long long maxA_B = LLONG_MIN;
+  long long maxA_B_C = 0;
+
+  for (int j = 1; j < n - 1; j++) {
+      maxA_B = max(maxA_B, maxA - v[j]);
+      maxA = max(maxA, (long long)v[j]);
+      maxA_B_C = max(maxA_B_C, maxA_B * (long long)v[j + 1]);
+  }
+
+  return maxA_B_C;
+}
+
+int main()
+{
+  vector<int> v = {1, 2, 3, 4};
+  cout << maximumTripletValue(v) << endl; // Output: 6
+  return 0;
+}
