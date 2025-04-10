0bf9b6bb59f3d3f98329df7f65b373d7c87b252e
Vinay Singh <vinaysthakur15@gmail.com>
2025-04-10 12:32:48 +0530

    Daily Coding Challenge DCC 10 April 2025 Count The Number of Powerful Integers Problem

---
+++ DailyCodingChallenge/April25/CountTheNumberofPowerfulIntergers.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long numberOfPowerfulInt(long long start, long long finish, int limit,string s) {
+  string t = to_string(start - 1);
+  long long f[20];
+  memset(f, -1, sizeof(f));
+
+  auto dfs = [&](this auto&& dfs, int pos, int lim) -> long long {
+    if (t.size() < s.size()) {
+      return 0;
+    }
+    if (!lim && f[pos] != -1) {
+      return f[pos];
+    }
+    if (t.size() - pos == s.size()) {
+      return lim ? s <= t.substr(pos) : 1;
+    }
+    long long ans = 0;
+    int up = min(lim ? t[pos] - '0' : 9, limit);
+    for (int i = 0; i <= up; ++i) {
+      ans += dfs(pos + 1, lim && i == (t[pos] - '0'));
+    }
+    if (!lim) {
+      f[pos] = ans;
+    }
+    return ans;
+  };
+
+  long long a = dfs(0, true);
+  t = to_string(finish);
+  memset(f, -1, sizeof(f));
+  long long b = dfs(0, true);
+  return b - a;
+}
+
+int main()
+{
+  long long start = 1;
+  long long finish = 100;
+  int limit = 10;
+  string s = "5";
+  
+  long long result = numberOfPowerfulInt(start, finish, limit, s);
+  cout << "Number of powerful integers: " << result << endl;
+
+  return 0;
+}
