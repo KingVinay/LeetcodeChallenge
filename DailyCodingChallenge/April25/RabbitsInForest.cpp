27d92fedcdabd3548b6bd92798c0070de3186ff8
Vinay Singh <vinaysthakur15@gmail.com>
2025-04-20 18:33:46 +0530

    Daily Coding Challenge DCC 20 April 2025 Rabbits In Forest Problem

---
+++ DailyCodingChallenge/April25/RabbitsInForest.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int numRabbits(vector<int>& answers) {
+  int ans = 0;
+  vector<int> count(1000);
+
+  for (const int answer : answers) {
+      if (count[answer] % (answer + 1) == 0)
+          ans += answer + 1;
+      ++count[answer];
+  }
+
+  return ans;
+}
+
+int main()
+{
+  vector<int> answers = {1, 1, 2};
+  int result = numRabbits(answers);
+  cout << "Number of rabbits: " << result << endl;
+
+  return 0;
+}
