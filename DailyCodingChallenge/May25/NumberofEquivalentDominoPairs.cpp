6e5b7351c6a53edac7f85a612f2cff03c707cfa0
Vinay Singh <vinaysthakur15@gmail.com>
2025-05-04 11:59:43 +0530

    Daily Coding Challenge DCC 04 May 2025 Number of Equivalent Domino Pairs Problem

---
+++ DailyCodingChallenge/May25/NumberofEquivalentDominoPairs.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+  int ans = 0;
+  unordered_map<int, int> count;
+
+  for (vector<int>& domino : dominoes) {
+      int key =
+          min(domino[0], domino[1]) * 10 + max(domino[0], domino[1]);
+      ans += count[key];
+      ++count[key];
+  }
+
+  return ans;
+}
+
+int main()
+{
+  vector<vector<int>> dominoes = {{1, 2}, {2, 1}, {3, 4}, {5, 6}, {4, 3}};
+  
+  int result = numEquivDominoPairs(dominoes);
+  cout << "Number of equivalent domino pairs: " << result << endl;
+
+  return 0;
+}
