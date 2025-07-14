137a424d5c9df7cb470a8ef4a80cb8abb43fe5b7
Vinay Singh <vinaysthakur15@gmail.com>
2025-07-14 23:24:23 +0530

    Daily Coding Challenge DCC 14 July 2025 Convert Binary Number in Linked List to Integer Problem

--- DailyCodingChallenge/July25/ConvertBinaryNumberinLinkedListtoInteger.cpp
+++ DailyCodingChallenge/July25/ConvertBinaryNumberinLinkedListtoInteger.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
 int getDecimalValue(ListNode* head) {
     int ans = 0;
 
